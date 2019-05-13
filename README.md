b42709ac6f01a9c3dea91b1d07c001b4a385ab06
Dave Helm <dahelm@microsoft.com>
2019-05-13 14:17:43 -0500

    Modified project to use Azure AD Service Principal.
    Modified project to use Azure AD Service Principal.

--- README.md
+++ README.md
@@ -1,65 +1,177 @@
+---
+services: active-directory
+platforms: Java
+author: davmhelm
+level: 300
+client: Java console daemon
+service: azure-storage
+endpoint: AAD V1
+---
+
 # Quick Start with Azure Storage SDK V10 for Java
+# Using Azure AD RBAC Authentication
+
+## About this Sample
+
+### Overview
+
+This sample demonstrates a Java console application calling Azure storage using Azure Active Directory RBAC.
+1. The Java web application uses the Active Directory Authentication Library for Java (ADAL4J) to obtain a JWT access token from Azure Active Directory (Azure AD)
+2. The access token is used as a bearer token to authenticate the user when calling Azure Storage.
+
+### Scenario
+
+This sample shows how to build a Java daemon app (confidential client) that uses OpenID Connect to authenticate against a single Azure Active Directory (Azure AD) tenant using ADAL4J. For more information about how the protocols work in this scenario and other scenarios, see [Authentication Scenarios for Azure AD](https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-authentication-scenarios).
+
 ## How to run this project
 
 ### Prerequisites
 
-If you don't have an Azure subscription, create a [free account](https://azure.microsoft.com/free/?WT.mc_id=A261C142F) before you begin.
+To run this sample, you'll need:
 
-### Create a storage account using the Azure portal
+- Working installation of [Java](http://www.oracle.com/technetwork/java/javase/downloads/index.html) and [Maven](https://maven.apache.org/)
+- An Internet connection
+- An Azure Subscription. If you don't have an Azure subscription, create a [free account](https://azure.microsoft.com/free/?WT.mc_id=A261C142F) before you begin.
+- An Azure Active Directory (Azure AD) tenant. For more information on how to get an Azure AD tenant, see [How to get an Azure AD tenant](https://azure.microsoft.com/en-us/documentation/articles/active-directory-howto-tenant/) 
+- A service principal in your Azure AD tenant.
 
-First, create a new general-purpose storage account to use for this tutorial. 
+### Step 1: Download Java (7 and above) for your platform
+
+To successfully use this sample, you need a working installation of [Java](http://www.oracle.com/technetwork/java/javase/downloads/index.html) and [Maven](https://maven.apache.org/).
+
+### Step 2:  Clone or download this repository
+
+From your shell or command line:
+
+`git clone https://github.com/davmhelm/storage-blobs-java-v10-quickstart.git`
+
+### Step 3:  Register the sample with your Azure Active Directory tenant
+
+You will be creating a `Service Principal` account for your app. To register the project, you can:
+
+#### Step 3.1: choose the Azure AD tenant where you want to create your applications
+
+As a first step you'll need to:
+
+1. Sign in to the [Azure portal](https://portal.azure.com).
+1. On the top bar, click on your account, and then on **Switch Directory**. 
+1. Once the *Directory + subscription* pane opens, choose the Active Directory tenant where you wish to register your application, from the *Favorites* or *All Directories* list.
+1. Click on **All services** in the left-hand nav, and choose **Azure Active Directory**.
+
+> In the next steps, you might need the tenant name (or directory name) or the tenant ID (or directory ID). These are presented in the **Properties**
+of the Azure Active Directory window respectively as *Name* and *Directory ID*
+
+#### Step 3.2 Register the app app (Native-Headless-Application)
+
+1. In the  **Azure Active Directory** pane, click on **App registrations** and choose **New registration**.
+1. Enter a friendly name for the application, for example `Storage-Quickstart-Java-Application`.
+1. For **Supported Account Types** leave at the default `Accounts in this organizational directory only`.
+1. For the *Redirect URI*, select `Web` as the type, and enter `https://localhost`.
+1. Click **Register** to create the application.
+1. In the succeeding page, Find the values for *Application (client) ID* and *Directory (tenant) ID* and copy them down. You'll need them to configure the properties file for this project.
+1. Configure an Application Key (Secret) for your application.
+    1. Click on **Certificates & Secrets**.
+    1. Under **Client secrets** click **+ New Client Secret**. Enter a description and leave the default expiration of 1 year, then click `Add`.
+    1. Copy the *client secret value* you just created to the same location as the *Application (client) ID* and *Directory (tenant) ID*.
+1. Configure Permissions for your application. 
+    1. Click on **API permissions**, and click **+ Add a Permission**.
+    1. Under **Select an API** click **APIs my organization uses**.
+    1. Type `Azure Storage` in the search box. Then, click on  **Delegated Permissions** and select **user_impersonation**.
+    1. Click on **Add Permissions**.
+
+### Step 4: Create a storage account using the Azure portal
+
+Create a new general-purpose storage account to use for this tutorial. 
 
 *  Go to the [Azure portal](https://portal.azure.com) and log in using your Azure account. 
 *  On the Hub menu, select **New** > **Storage** > **Storage account - blob, file, table, queue**. 
-*  Enter a name for your storage account. The name must be between 3 and 24 characters in length and may contain numbers and lowercase letters only. It must also be unique.
+*  Enter a name for your storage account. The name must be between 3 and 24 characters in length and may contain numbers and lowercase letters only. It must also be unique. Make note of this name for later.
 *  Set `Deployment model` to **Resource manager**.
-*  Set `Account kind` to **General purpose**.
+*  Set `Account kind` to **General purpose (v2)**.
 *  Set `Performance` to **Standard**. 
 *  Set `Replication` to **Locally Redundant storage (LRS)**.
-*  Set `Storage service encryption` to **Disabled**.
-*  Set `Secure transfer required` to **Disabled**.
+*  Leave `Secure transfer required` at the default value **Enabled**.
 *  Select your subscription. 
 *  For `resource group`, create a new one and give it a unique name. 
 *  Select the `Location` to use for your storage account.
-*  Check **Pin to dashboard** and click **Create** to create your storage account. 
+*  Click **Create** to create your storage account. 
 
-After your storage account is created, it is pinned to the dashboard. Click on it to open it. Under SETTINGS, click **Access keys**. Select a key and copy the **key1** to the clipboard, then paste it into Notepad for later use.
+### Step 5: Assign RBAC permissions for service srincipal to storage account using the Azure portal
 
-### Set credentials in environment variables 
+In the storage account you just created:
 
-Linux
+* From the left-nav bar, click **Access control (IAM)**.
+* Click **+ Add** > **Add role assignment**.
+* In the **Role** field, type `Storage Blob Data Contributor`.
+* Leave **Assign access to** set to `Azure AD user, group, or service principal`.
+* In the **Select** field, type the name of the App Registration you created in **Step 3** above, ex `Storage-Quickstart-Java-Application` and select the member from the filtered list.
+* Click **Save**. These settings changes can take up to 5 minutes to take effect.
+
+### Step 6: Set credentials in a properties file
+
+This Quickstart uses a Java properties key/value pair file in order to connect to Azure AD and the Azure Storage account at runtime. An example properties file is included at `secrets/azureauth.properties.template`. 
+
+It is recommended to keep the populated file in a safe place, and ensure it does not get uploaded into source control (using `.gitignore` for example).
+
+With the exception of authorityURL, fill in the rest of the fields in the file with values taken from the instructions above.
+
+If anything you're filling in uses a [special character](https://docs.oracle.com/javase/7/docs/api/java/util/Properties.html#store%28java.io.Writer,%20java.lang.String%29) like `:` or `=`, make sure to escape it first using the escape character `\` (ex. `:` becomes `\:`).
+
 ```
-export AZURE_STORAGE_ACCOUNT="<youraccountname>"
-export AZURE_STORAGE_ACCESS_KEY="<youraccountkey>"
+# your Azure AD Tenant ID 'aaaa0000-bbbb-1111-cccc-2222dddd3333'
+tenantId=
+
+# your Service principal client ID 'aaaa0000-bbbb-1111-cccc-2222dddd3333'
+clientId=
+
+# your Service principal secret (AKA client secret, auth key, etc.)
+clientSecret=
+
+# Leave this alone unless you're on a different cloud from Commercial
+authorityUrl=https\://login.microsoftonline.com
+
+# Storage account name (just the name, app takes care of the rest of the URI)
+storageAccountName=
 ```
 
-Windows
+### Step 7: Set path to properties file in environment variable
+
+From your shell or command line:
+
+#### Linux
 ```
-setx AZURE_STORAGE_ACCOUNT "<youracountname>"
-setx AZURE_STORAGE_ACCESS_KEY "<youraccountkey>"
+export STORAGE_QUICKSTART_AUTH_PROPERTIES="/path/to/azureauth.properties"
 ```
 
-At this point, you can run this application using maven: `mvn compile exec:java`. It creates its own file to upload and download, and then cleans up after itself by deleting everything at the end. 
-
+#### Windows
 ```
-mvn compile exec:java
+set STORAGE_QUICKSTART_AUTH_PROPERTIES="<driveletter>:\path\to\azureauth.properties"
+```
+
+### Step 8: Build and Run
+
+From your shell or command line:
+
+at this point you can build this application using maven: `mvn package`. Then, run the package using `java -jar <packageFile>`. It creates its own file to upload and download, and then cleans up after itself by deleting everything at the end. 
+
+#### Linux
+```
+mvn package
+java -jar target/Quickstart-jar-with-dependencies.jar
+```
+
+#### Windows
+```
+mvn package
+java -jar target\Quickstart-jar-with-dependencies.jar
 ```
 
 ## Resources
 * [Azure Storage SDK v10 for Java](https://github.com/azure/azure-storage-java/tree/vNext)
-* [API Reference](https://docs.microsoft.com/en-us/java/api/storage/client?view=azure-java-preview)
+* [Azure Storage Java SDK Reference](https://docs.microsoft.com/en-us/java/api/storage/client?view=azure-java-preview)
+* [Authenticate with Azure Active Directory to Azure Blob Storage](https://docs.microsoft.com/en-us/azure/storage/common/storage-auth-aad-app)
+* [Manage access to Azure Resources using RBAC](https://docs.microsoft.com/en-us/azure/role-based-access-control/role-assignments-portal)
 
-# Contributing
+# Disclaimer
 
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.microsoft.com.
-
-When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
-
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
-
+The information contained in this quickstart and any accompanying materials (including, but not limited to, scripts, sample code, etc.) are provided "AS-IS" and "WITH ALL FAULTS." Any estimated pricing information is provided solely for demonstration purposes and does not represent final pricing and Microsoft assumes no liability arising from your use of the information. Microsoft makes NO GUARANTEES OR WARRANTIES OF ANY KIND, WHETHER EXPRESSED OR IMPLIED, in providing this information, including any pricing information.
--- src/main/java/quickstart/Quickstart.java
+++ src/main/java/quickstart/Quickstart.java
@@ -3,10 +3,13 @@ package quickstart;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Writer;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.AsynchronousFileChannel;
@@ -14,15 +17,30 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.InvalidKeyException;
+import java.time.OffsetDateTime;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import javax.naming.ServiceUnavailableException;
+
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+import com.microsoft.aad.adal4j.UserAssertion;
+import com.microsoft.azure.storage.blob.AnonymousCredentials;
 import com.microsoft.azure.storage.blob.BlobRange;
+import com.microsoft.azure.storage.blob.BlobSASPermission;
 import com.microsoft.azure.storage.blob.BlockBlobURL;
 import com.microsoft.azure.storage.blob.ContainerURL;
 import com.microsoft.azure.storage.blob.ListBlobsOptions;
 import com.microsoft.azure.storage.blob.PipelineOptions;
+import com.microsoft.azure.storage.blob.SASProtocol;
+import com.microsoft.azure.storage.blob.ServiceSASSignatureValues;
 import com.microsoft.azure.storage.blob.ServiceURL;
-import com.microsoft.azure.storage.blob.SharedKeyCredentials;
 import com.microsoft.azure.storage.blob.StorageURL;
+import com.microsoft.azure.storage.blob.TokenCredentials;
 import com.microsoft.azure.storage.blob.TransferManager;
 import com.microsoft.azure.storage.blob.models.BlobItem;
 import com.microsoft.azure.storage.blob.models.ContainerCreateResponse;
@@ -34,6 +52,7 @@ import io.reactivex.*;
 import io.reactivex.Flowable;
 
 public class Quickstart {
+
     static File createTempFile() throws IOException {
 
         // Here we are creating a temporary file to use for download and upload to Blob storage
@@ -130,29 +149,77 @@ public class Quickstart {
         });
     }
 
+    private static AuthenticationResult getAccessTokenFromServicePrincipal(String authority,
+            String resourceUri, String clientId, String clientSecret) throws Exception {
+        AuthenticationContext context;
+        AuthenticationResult result;
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(1);
+            context = new AuthenticationContext(authority, false, service);
+            ClientCredential myClientCredential = new ClientCredential(clientId, clientSecret);
+            Future<AuthenticationResult> future = context.acquireToken(resourceUri, myClientCredential, null);
+            result = future.get();
+        } finally {
+            service.shutdown();
+        }
+
+        if (result == null) {
+            throw new ServiceUnavailableException(
+                    "authentication result was null");
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) throws java.lang.Exception{
+
         ContainerURL containerURL;
 
         // Creating a sample file to use in the sample
         File sampleFile = null;
 
-        try {
-            sampleFile = createTempFile();
+        Properties prop = new Properties();
+        String AUTHORITY_URI = "";
+        String CLIENT_ID = "";
+        String CLIENT_SECRET = "";
+        String AZURE_STORAGE_ACCOUNT = "";
+        final String RESOURCE_URI = "https://storage.azure.com";
 
+        try {
+            // Load configuration from properties file
+            InputStream propfileInput = new FileInputStream(System.getenv("STORAGE_QUICKSTART_AUTH_PROPERTIES"));
+            prop.load(propfileInput);
+            //prop.forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+            AUTHORITY_URI = prop.getProperty("authorityUrl") + "/" + prop.getProperty("tenantId") + "/";
+            CLIENT_ID = prop.getProperty("clientId");
+            CLIENT_SECRET = prop.getProperty("clientSecret");
+            AZURE_STORAGE_ACCOUNT = prop.getProperty("storageAccountName");
+            propfileInput.close();
+            //final String RESOURCE_URI = "https://storage.azure.com";
+        } catch (Exception e) {
+            System.out.println("Problem found with properties file. Check your path and format for \n" +
+                    "environment variable \"STORAGE_QUICKSTART_AUTH_PROPERTIES\" and try again.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        try {
+            // Prepare demo file
+            sampleFile = createTempFile();
             File downloadedFile = File.createTempFile("downloadedFile", ".txt");
 
-            // Retrieve the credentials and initialize SharedKeyCredentials
-            String accountName = System.getenv("AZURE_STORAGE_ACCOUNT");
-            String accountKey = System.getenv("AZURE_STORAGE_ACCESS_KEY");
-
-            // Create a ServiceURL to call the Blob service. We will also use this to construct the ContainerURL
-            SharedKeyCredentials creds = new SharedKeyCredentials(accountName, accountKey);
+            // Authenticate to Azure Storage via AAD Service Principal
+            AuthenticationResult result = getAccessTokenFromServicePrincipal(AUTHORITY_URI, RESOURCE_URI, CLIENT_ID, CLIENT_SECRET);
+            TokenCredentials creds = new TokenCredentials(result.getAccessToken());
+                        
             // We are using a default pipeline here, you can learn more about it at https://github.com/Azure/azure-storage-java/wiki/Azure-Storage-Java-V10-Overview
-            final ServiceURL serviceURL = new ServiceURL(new URL("https://" + accountName + ".blob.core.windows.net"), StorageURL.createPipeline(creds, new PipelineOptions()));
-
+            final ServiceURL serviceURL = new ServiceURL(new URL("https://" + AZURE_STORAGE_ACCOUNT + ".blob.core.windows.net"), StorageURL.createPipeline(creds, new PipelineOptions()));
+            
             // Let's create a container using a blocking call to Azure Storage
             // If container exists, we'll catch and continue
-            containerURL = serviceURL.createContainerURL("quickstart");
+            String containerName = "quickstart";
+            containerURL = serviceURL.createContainerURL(containerName);
 
             try {
                 ContainerCreateResponse response = containerURL.create(null, null, null).blockingGet();
@@ -166,11 +233,12 @@ public class Quickstart {
             }
 
             // Create a BlockBlobURL to run operations on Blobs
-            final BlockBlobURL blobURL = containerURL.createBlockBlobURL("SampleBlob.txt");
+            String blobName = "SampleBlob.txt";
+            final BlockBlobURL blobURL = containerURL.createBlockBlobURL(blobName);
 
             // Listening for commands from the console
             System.out.println("Enter a command");
-            System.out.println("(P)utBlob | (L)istBlobs | (G)etBlob | (D)eleteBlobs | (E)xitSample");
+            System.out.println("(P)utBlob | (L)istBlobs | (G)etBlob | (D)eleteBlobs | (E)xitSample"); // + " | (C)reateSasSignature"
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             while (true) {
@@ -178,7 +246,7 @@ public class Quickstart {
                 System.out.println("# Enter a command : ");
                 String input = reader.readLine();
 
-                switch(input){
+                switch(input.toUpperCase()){
                     case "P":
                         System.out.println("Uploading the sample file into the container: " + containerURL );
                         uploadFile(blobURL, sampleFile);
@@ -202,17 +270,80 @@ public class Quickstart {
                         downloadedFile.delete();
                         System.exit(0);
                         break;
+// This doesn't work because SAS signatures are keyed off the storage account shared keys
+//                  case "C":
+//                      System.out.println("Uploading the sample file into the container: " + containerURL );
+//                      uploadFile(blobURL, sampleFile);
+//
+//                      // Based on Sample Code here:
+//                      // https://docs.microsoft.com/en-us/java/api/com.microsoft.azure.storage.blob.servicesassignaturevalues?view=azure-java-stable#sample-code
+//                      
+//                      /*
+//                       * Actions taken by this block are done already.
+//                       * We're not using a snapshot ID here.
+//
+//                      // Use your Storage account's name and key to create a credential object; this is required to sign a SAS.
+//                      credential = new SharedKeyCredentials(getAccountName(), getAccountKey());
+//
+//                      // This is the name of the container and blob that we're creating a SAS to.
+//                      String containerName = "mycontainer"; // Container names require lowercase.
+//                      String blobName = "HelloWorld.txt"; // Blob names can be mixed case.
+//                      String snapshotId = "2018-01-01T00:00:00.0000000Z"; // SAS can be restricted to a specific snapshot
+//                      
+//                      */
+//
+//                      /*
+//                      Set the desired SAS signature values and sign them with the shared key credentials to get the SAS query
+//                      parameters.
+//                      */
+//                      ServiceSASSignatureValues values = new ServiceSASSignatureValues()
+//                              .withProtocol(SASProtocol.HTTPS_ONLY) // Users MUST use HTTPS (not HTTP).
+//                              .withExpiryTime(OffsetDateTime.now().plusDays(2)) // 2 days before expiration.
+//                              .withContainerName(containerName)
+//                              .withBlobName(blobName);
+//                              //.withSnapshotId(snapshotId);
+//
+//                      /*
+//                      To produce a container SAS (as opposed to a blob SAS), assign to Permissions using ContainerSASPermissions, and
+//                      make sure the blobName and snapshotId fields are null (the default).
+//                      */
+//                      BlobSASPermission permission = new BlobSASPermission()
+//                              .withRead(true)
+//                              .withAdd(true)
+//                              .withWrite(true);
+//                      values.withPermissions(permission.toString());
+//                      
+//                      SASQueryParameters serviceParams = values.generateSASQueryParameters(creds);
+//
+//                      // Calling encode will generate the query string.
+//                      String encodedParams = serviceParams.encode();
+//                      // Colons are not safe characters in a URL; they must be properly encoded.
+//                      //snapshotId = snapshotId.replace(":", "%3A");
+//
+//                      String urlToSendToSomeone = String.format(Locale.ROOT, "https://%s.blob.core.windows.net/%s/%s?%s&%s",
+//                              getAccountName(), containerName, blobName, encodedParams);
+//                      // At this point, you can send the urlToSendSomeone to someone via email or any other mechanism you choose.
+//
+//                      // ***************************************************************************************************
+//
+//                      // When someone receives the URL, the access the SAS-protected resource with code like this:
+//                      URL u = new URL(urlToSendToSomeone);
+//
+//                      /*
+//                      Create a BlobURL object that wraps the blobURL (and its SAS) and a pipeline. When using SAS URLs,
+//                      AnonymousCredentials are required.
+//                      */
+//                      BlockBlobURL bURL = new BlockBlobURL(u,
+//                              StorageURL.createPipeline(new AnonymousCredentials(), new PipelineOptions()));
+//                      // Now, you can use this blobURL just like any other to make requests of the resource.
+//                      break; 
                     default:
                         break;
                 }
             }
-        } catch (InvalidKeyException e) {
-            System.out.println("Invalid Storage account name/key provided");
-        } catch (MalformedURLException e) {
-            System.out.println("Invalid URI provided");
         } catch (RestException e){
             System.out.println("Service error returned: " + e.response().statusCode() );
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
